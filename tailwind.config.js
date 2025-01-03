46c61c5307704ab87467f1e943c39942cc5bec60
81andie <133395577+81andie@users.noreply.github.com>
2025-01-03 21:35:09 +0100

    cambios instalacion tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.html",
+    './index.html',
+    './home.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
