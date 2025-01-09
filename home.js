d676e70c42abbead5c7ac5a24c04ec157201e13f
81andie <133395577+81andie@users.noreply.github.com>
2025-01-09 21:08:28 +0100

    Añadiendo la funcionalidad para que los links esten activos o no y poniendoles una animacion

---
+++ home.js
@@ -0,0 +1,17 @@
+
+const activePage = window.location.pathname;
+
+
+const navLinks = document.querySelectorAll('nav a').forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active')
+    }
+})
+
+
+
+
+
+
+
+
